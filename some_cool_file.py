5dda78542948a08e87cc3d4956fb3e5d43ac2ab9
Udokaamos <udokaamos@gmail.com>
2023-08-11 13:57:25 +0200

    added .gitignore

---
+++ some_cool_file.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # loading every .env file in the project
+api_key=os.getenv('API_KEY1')
+sk=os.getenv('SECRET_KEY')
+jk=os.getenv('JOHN_KEY')
+print(api_key)
+print(sk)
+print(jk)
+
+print(jk.split())
